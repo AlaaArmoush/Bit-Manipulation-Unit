7a361c8b85a5d42ce8de1268b4b244b29d3d6d52
Alaa Armoush <armoush2004@gmail.com>
2026-09-13 14:33:54 +0300

    connect BMU environment components

--- tb/env/bmu_environment.sv
+++ tb/env/bmu_environment.sv
@@ -1,30 +1,42 @@
 class bmu_environment extends uvm_env;
   `uvm_component_utils(bmu_environment)
 
-  bmu_agent agent;
-  bmu_scoreboard scoreboard;
+  bmu_agent               agent;
+  bmu_scoreboard          scoreboard;
   bmu_coverage_subscriber coverage_subscriber;
 
-  function new(string name = "bmu_environment", uvm_component parent);
+  function new(string name = "bmu_environment", uvm_component parent = null);
     super.new(name, parent);
-  endfunction
+  endfunction : new
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    scoreboard = bmu_scoreboard::type_id::create("agent", this);
+
+    agent = bmu_agent::type_id::create("agent", this);
+
+    scoreboard = bmu_scoreboard::type_id::create("scoreboard", this);
+
     coverage_subscriber = bmu_coverage_subscriber::type_id::create("coverage_subscriber", this);
 
-    if (agent == null) `uvm_fatal("NO_AGENT", "Failed to create the BMU agent")
+    if (agent == null) begin
+      `uvm_fatal("NO_AGENT", "Failed to create the BMU agent")
+    end
 
-    if (scoreboard == null) `uvm_fatal("NO_SCOREBOARD", "Failed to create the BMU scoreboard")
+    if (scoreboard == null) begin
+      `uvm_fatal("NO_SCOREBOARD", "Failed to create the BMU scoreboard")
+    end
 
-    if (coverage_subscriber == null)
+    if (coverage_subscriber == null) begin
       `uvm_fatal("NO_COVERAGE", "Failed to create the BMU coverage subscriber")
+    end
   endfunction : build_phase
 
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
+
     agent.monitor.analysis_port.connect(scoreboard.analysis_imp);
+
     agent.monitor.analysis_port.connect(coverage_subscriber.analysis_export);
   endfunction : connect_phase
+
 endclass
